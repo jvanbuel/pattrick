c8cdb95866aeca8dcbc9eee75f6338ad355345d9
Jan <janvanbuel@hotmail.com>
2022-10-06 22:55:49 +0200

    progress on list

--- src/pat.rs
+++ src/pat.rs
@@ -4,8 +4,8 @@ use azure_core::auth::TokenResponse;
 use azure_identity::token_credentials::AzureCliCredential;
 use azure_identity::token_credentials::TokenCredential;
 use reqwest::Client;
+use reqwest::IntoUrl;
 use reqwest::Method;
-use reqwest::Url;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -14,7 +14,7 @@ use crate::args::ListOpts;
 const AZURE_DEVOPS_PAT_URL: &str = "https://vssps.dev.azure.com/imec-int/_apis/tokens/pats";
 const API_VERSION: &str = "7.1-preview.1";
 
-#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct PatToken {
     pub display_name: String,
@@ -23,10 +23,10 @@ pub struct PatToken {
     pub target_accounts: Vec<String>,
     pub valid_from: String,
     pub authorization_id: String,
-    pub token: String,
+    pub token: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct ListTokenResponse {
     pub continuation_token: Option<String>,
@@ -39,7 +39,10 @@ pub struct PatTokenManager {
 }
 
 impl PatTokenManager {
-    fn base_request(&self, method: Method, url: Url) -> reqwest::RequestBuilder {
+    fn base_request<U>(&self, method: Method, url: U) -> reqwest::RequestBuilder
+    where
+        U: IntoUrl,
+    {
         self.client
             .request(method, url)
             .header("Authorization", format!("Bearer {}", self.ad_token))
@@ -49,22 +52,21 @@ impl PatTokenManager {
     }
     pub async fn list_pat_tokens(
         &self,
-        list_opts: &ListOpts,
+        _list_opts: &ListOpts,
     ) -> Result<Vec<PatToken>, Box<dyn Error>> {
         let response = self
-            .client
-            .get(AZURE_DEVOPS_PAT_URL)
-            .bearer_auth(&self.ad_token)
-            .query(&[("api-version", API_VERSION)])
+            .base_request(Method::GET, AZURE_DEVOPS_PAT_URL)
             .send()
             .await?;
 
-        println!("{:?}", response.text().await?);
+        let lt_response = response.json::<ListTokenResponse>().await?;
+
+        println!("{:?}", &lt_response);
 
         Ok(vec![PatToken::default()])
     }
 
-    pub async fn create_pat_token(self, valid_to: String) -> Result<PatToken, Box<dyn Error>> {
+    pub async fn create_pat_token(self, _valid_to: String) -> Result<PatToken, Box<dyn Error>> {
         let response = self
             .client
             .post(AZURE_DEVOPS_PAT_URL)
