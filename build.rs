ea977d05564ce02df7be75508bd337627d44e787
Jan <janvanbuel@hotmail.com>
2022-12-31 15:35:15 +0100

    bugfix: write fig spec to target so that docs.rs can succsefully build

--- build.rs
+++ build.rs
@@ -1,8 +1,19 @@
+use std::{env, fmt::Error};
+
 use clap::CommandFactory;
 use clap_complete_fig::Fig;
 use pattrick_clap::Cli;
 
-fn main() {
-    clap_complete::generate_to(Fig, &mut Cli::command(), "pattrick", "fig_spec")
+fn main() -> Result<(), Error> {
+    let outdir = if let Ok(outdir) = env::var("OUT_DIR") {
+        outdir
+    } else {
+        String::from("fig_spec")
+    };
+    let path = clap_complete::generate_to(Fig, &mut Cli::command(), "pattrick", outdir)
         .expect("Unable to generate Fig spec");
+
+    println!("cargo:warning=completion file is generated: {path:?}");
+
+    Ok(())
 }
